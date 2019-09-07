d3b76ef3003c31b535b03cd4dead82a4db20a59b
rayGreycloud <rayGreycloud@gmail.com>
2019-09-07 07:16:46 -0700

    correct stripe route

--- server.js
+++ server.js
@@ -38,8 +38,9 @@ app.post('/payment', (req, res) => {
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if (stripeErr) {
       res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
     }
-    res.status(200).send({ success: stripeRes });
   });
 });
 
